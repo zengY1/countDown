fa69a9395819d79706b0e2f182ae733939d6077f
zengdp <zengdp0516@163.com>
2021-09-07 13:42:01 +0800

    添加设置信息的说明

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # countDown README
-
-This is the README for your extension "countDown". After writing up a brief description, we recommend including the following sections.
+倒计时提醒小助手
 ### 1.0.0
 
 countDown ...
--- extension.js
+++ extension.js
@@ -1,24 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 var activeStatusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
-var showTime = ''
 var activeTitle = ''
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-
 	getDate()
 	activeStatusBar.show()
 }
@@ -67,7 +53,6 @@ function getDate() {
 		}, 1000)
 	} else {
 		activeStatusBar.text = activeTitle
-		// vscode.window.showInformationMessage(activeTitle)
 	}
 }
 function formatSeconds(value) {
@@ -86,7 +71,6 @@ function formatSeconds(value) {
 	const minuteTitle = minute > 0 ? `${minute}分钟` : ''
 	activeStatusBar.text = `距离${activeTitle}还有${hourTitle}${minuteTitle}${second}秒`
 }
-
 
 function deactivate() {
 
